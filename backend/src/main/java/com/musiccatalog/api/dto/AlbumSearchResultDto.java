89310f028c37a0045dd7c83f35600f3c28d5fd84
Sandeep Reddy <sandeepreddy0485@gmail.com>
2026-07-31 11:31:17 +0530

    Fix Jackson JSON property mapping in AlbumSearchResultDto to properly display iTunes album metadata in React frontend

--- backend/src/main/java/com/musiccatalog/api/dto/AlbumSearchResultDto.java
+++ backend/src/main/java/com/musiccatalog/api/dto/AlbumSearchResultDto.java
@@ -1,17 +1,27 @@
 package com.musiccatalog.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class AlbumSearchResultDto {
 
+    @JsonAlias("collectionId")
     private Long appleCatalogId;
+    
+    @JsonAlias("collectionName")
     private String title;
+    
     private String artistName;
+    
+    @JsonAlias("primaryGenreName")
     private String genre;
+    
     private String releaseDate;
+    
     private Integer trackCount;
+    
+    @JsonAlias("artworkUrl100")
     private String artworkUrl;
 
     public AlbumSearchResultDto() {}
@@ -27,43 +37,30 @@ public class AlbumSearchResultDto {
         this.artworkUrl = artworkUrl;
     }
 
-    @JsonProperty("collectionId")
     public Long getAppleCatalogId() { return appleCatalogId; }
     
-    @JsonProperty("collectionId")
     public void setAppleCatalogId(Long appleCatalogId) { this.appleCatalogId = appleCatalogId; }
 
-    @JsonProperty("collectionName")
     public String getTitle() { return title; }
     
-    @JsonProperty("collectionName")
     public void setTitle(String title) { this.title = title; }
 
-    @JsonProperty("artistName")
     public String getArtistName() { return artistName; }
     
-    @JsonProperty("artistName")
     public void setArtistName(String artistName) { this.artistName = artistName; }
 
-    @JsonProperty("primaryGenreName")
     public String getGenre() { return genre; }
     
-    @JsonProperty("primaryGenreName")
     public void setGenre(String genre) { this.genre = genre; }
 
-    @JsonProperty("releaseDate")
     public String getReleaseDate() { return releaseDate; }
     
-    @JsonProperty("releaseDate")
     public void setReleaseDate(String releaseDate) { this.releaseDate = releaseDate; }
 
-    @JsonProperty("trackCount")
     public Integer getTrackCount() { return trackCount; }
     
-    @JsonProperty("trackCount")
     public void setTrackCount(Integer trackCount) { this.trackCount = trackCount; }
 
-    @JsonProperty("artworkUrl100")
     public String getArtworkUrl() {
         if (artworkUrl == null || artworkUrl.isBlank()) {
             return null;
@@ -71,6 +68,5 @@ public class AlbumSearchResultDto {
         return artworkUrl.replace("http://", "https://");
     }
     
-    @JsonProperty("artworkUrl100")
     public void setArtworkUrl(String artworkUrl) { this.artworkUrl = artworkUrl; }
 }
